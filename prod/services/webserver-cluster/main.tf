b84a026b6bafc17fd70b864cff18acb615168e8e
DUDINGDDI <sakmsik98@naver.com>
2024-08-05 22:48:19 -0400

    Feat: prod, stage add module variance(bucket, key)

--- prod/services/webserver-cluster/main.tf
+++ prod/services/webserver-cluster/main.tf
@@ -1,6 +1,10 @@
 provider "aws" {
  region = "ap-northeast-2"
 }
+
 module "webserver_cluster" {
  source = "../../../modules/services/webserver-cluster"
+ cluster_name = "webservers-prod"
+ db_remote_state_bucket = "cloudwave-cj-ddd"
+ db_remote_state_key = "prod/data-stores/mysql/terraform.tfstate"
 }
