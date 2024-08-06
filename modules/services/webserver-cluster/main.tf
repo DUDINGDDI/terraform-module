37f7b0d477ca0ad130e8eb52b7f9199fc308cd98
DUDINGDDI <sakmsik98@naver.com>
2024-08-05 22:57:38 -0400

    instance, min, max Feat: modules main.tf -> instance, min max change var Feat: stage main.tf -> add Variable

--- modules/services/webserver-cluster/main.tf
+++ modules/services/webserver-cluster/main.tf
@@ -69,13 +69,13 @@ resource "aws_lb_target_group" "asg" {
 
 resource "aws_launch_configuration" "example" {
   image_id = "ami-0f3a440bbcff3d043" 
-  instance_type = "t3.micro"
+  instance_type = var.instance_type
   security_groups = [aws_security_group.instance.id]
 
   user_data = templatefile("user-data.sh", {
-  server_port = var.server_port
-  db_address = data.terraform_remote_state.db.outputs.address
-  db_port = data.terraform_remote_state.db.outputs.port
+    server_port = var.server_port
+    db_address = data.terraform_remote_state.db.outputs.address
+    db_port = data.terraform_remote_state.db.outputs.port
   })
 
   lifecycle {
@@ -87,8 +87,9 @@ resource "aws_autoscaling_group" "example" {
   launch_configuration = aws_launch_configuration.example.name 
   vpc_zone_identifier = data.aws_subnets.default.ids
   target_group_arns = [aws_lb_target_group.asg.arn]
-  min_size = 2
-  max_size = 10
+  health_check_type = "ELB"
+  min_size = var.min_size
+  max_size = var.max_size
   tag {
     key = "Name"
     value = "terraform-asg-example"
